6eaed7001b5a904ad86e3598ddd797eb82050e18
ctdk <jbingham@gmail.com>
2015-01-12 00:01:56 -0800

    Don't need to deref & re-ref

--- sbinfo.go
+++ sbinfo.go
@@ -28,12 +28,12 @@ func ReadExt2Superblock(devPath string) (*Ext2Sb, error) {
 		return nil, err
 	}
 	sbRaw := buf[1024:]
-	sb := *(*Ext2Sb)(unsafe.Pointer(&sbRaw[0]))
+	sb := (*Ext2Sb)(unsafe.Pointer(&sbRaw[0]))
 	// This needs to be able to check if it's running on a bigendian system
 	// and switch up the sb values as needed.
 	if sb.SMagic != ext2Magic {
 		sbErr := fmt.Errorf("Bad magic number for %s", devPath)
 		return nil, sbErr
 	}
-	return &sb, nil
+	return sb, nil
 }
